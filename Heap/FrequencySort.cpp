a8c3002c9544bb5867ec79912ea28b37ef33befd
Nikhar Gangwar <62029765+nikhargangwar@users.noreply.github.com>
2022-01-05 15:53:13 +0530

    initial commit

--- Heap/FrequencySort.cpp
+++ Heap/FrequencySort.cpp
@@ -2,36 +2,36 @@
 //If multiple values have the same frequency, sort them in decreasing order.
 
 
-// class Solution {
-// public:
-//     vector<int> frequencySort(vector<int>& nums) {
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
         
-//         map<int,int> map;
-//         priority_queue<pair<int,int>,vector<int>, greater<int>> min_heap;
-//         vector<int> v;
+        map<int,int> map;
+        priority_queue<pair<int,int>,vector<int>, greater<int>> min_heap;
+        vector<int> v;
         
-//         for(int i=0;i<nums.size();i++)
-//         {
-//             map[nums[i]]++;
-//         }
+        for(int i=0;i<nums.size();i++)
+        {
+            map[nums[i]]++;
+        }
         
-//         for(auto i=map.begin();i!=map.end();i++)
-//         {
-//             min_heap.push({i->second,i->first});
+        for(auto i=map.begin();i!=map.end();i++)
+        {
+            min_heap.push({i->second,i->first});
             
-//         }
+        }
         
-//         while(min_heap.size()>0)
-//         {
-//             for(int i=0;i<min_heap.top().first;i++)
-//             {
-//                 v.push_back(min_heap.top().second);
-//             }
+        while(min_heap.size()>0)
+        {
+            for(int i=0;i<min_heap.top().first;i++)
+            {
+                v.push_back(min_heap.top().second);
+            }
             
-//             min_heap.pop();
-//         }
+            min_heap.pop();
+        }
         
-//         return v;
+        return v;
         
-//     }
-// };
+    }
+};
