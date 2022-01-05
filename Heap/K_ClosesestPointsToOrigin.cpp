55f0692306834468455a6f28d412318ca7c5fd40
Nikhar Gangwar <62029765+nikhargangwar@users.noreply.github.com>
2022-01-05 15:57:12 +0530

    code added

--- Heap/K_ClosesestPointsToOrigin.cpp
+++ Heap/K_ClosesestPointsToOrigin.cpp
@@ -11,31 +11,31 @@
 
 
 
-// class Solution {
-// public:
-//     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         
-//         priority_queue<pair<int, pair<int,int>>> max_heap;
-//         vector<vector<int>> v;
+        priority_queue<pair<int, pair<int,int>>> max_heap;
+        vector<vector<int>> v;
         
-//         for(int i=0;i<points.size();i++)
-//         {
+        for(int i=0;i<points.size();i++)
+        {
             
-//     max_heap.push({points[i][0]*points[i][0]+points[i][1]*points[i][1],{points[i][0],points[i][1]}});
+    max_heap.push({points[i][0]*points[i][0]+points[i][1]*points[i][1],{points[i][0],points[i][1]}});
             
-//             if(max_heap.size()>k)
-//             {
-//                 max_heap.pop();
-//             }
-//         }
+            if(max_heap.size()>k)
+            {
+                max_heap.pop();
+            }
+        }
         
-//         while(max_heap.size()>0)
-//         {
-//             v.push_back({max_heap.top().second.first,max_heap.top().second.second});
-//             max_heap.pop();
-//         }
+        while(max_heap.size()>0)
+        {
+            v.push_back({max_heap.top().second.first,max_heap.top().second.second});
+            max_heap.pop();
+        }
         
-//         return v;
+        return v;
         
-//     }
-// };
+    }
+};
